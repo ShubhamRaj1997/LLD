63d6f1fb56544bc75ad91009b39d206a28e5f19b
ShubhamRaj1997 <shubhamrajputcool@gmail.com>
2021-09-30 06:11:15 +0530

    small fix in comment.py

--- stackoverflow/models/comment.py
+++ stackoverflow/models/comment.py
@@ -5,7 +5,8 @@ class Comment(BaseArticle):
     def __init__(self, comment_text, created_by):
         super(Comment, self).__init__(comment_text, created_by)
 
-class CommentV2():
+
+class CommentV2(object):
     def __init__(self, comment_text, created_by):
         self.__base_article = BaseArticle(comment_text, created_by)
 
